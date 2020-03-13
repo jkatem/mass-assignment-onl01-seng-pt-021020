41a681f1c3208612b70272c8446b463beab81deb
Jen Miao <jenmiao@gmail.com>
2020-03-13 19:44:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
   attr_accessor :name, :eye_color, :weight, :t_shirt_size, :wrist_size, :pant_width
 
   def initialize(attributes)
-    attributes.each {|key, value| self.send(('#{key}='), value)}
-  end 
+    attributes.each {|key, value| self.send("#{key}=", value)}
+  end
 
 end
